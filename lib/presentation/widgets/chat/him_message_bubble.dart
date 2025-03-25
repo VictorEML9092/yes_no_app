95505b2b80b304172e28b3540074969e1583a4a3
Victor Eduardo Mendoza-Lopez <victormendoza2008@outlook.com>
2025-03-25 17:18:57 -0600

    Application Message Bubble (Him)

---
+++ lib/presentation/widgets/chat/him_message_bubble.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class HimMessageBubble extends StatelessWidget {
+  const HimMessageBubble({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+              color: colors.secondary, borderRadius: BorderRadius.circular(20)),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text(
+              'Hola Mundo',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+
+        // Todo: imagen
+
+        _ImageBubble(),
+
+        const SizedBox(
+          height: 10,
+        )
+      ],
+    );
+  }
+}
+
+class _ImageBubble extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Image.network(
+        'https://yesno.wtf/assets/no/2-101be1e3d8a0ed407c4e3c001ef8fa66.gif',
+        width: size.width * 0.7,
+        height: 150,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+}
